e47a5f59ddef8fd012d52fbd006b9c892572d089
Heinz-Günter Hermanns <hgh.dev@t-online.de>
2026-08-10 23:26:48 +0200

    sdl3

--- dev/src/sdl/sdl3/src/gamepad/gamepad-events.cpp
+++ dev/src/sdl/sdl3/src/gamepad/gamepad-events.cpp
@@ -1,44 +1,18 @@
 // https://github.com/hghpublic/libsdl-org-SDL/blob/dev/examples/0/examples/input/03-gamepad-polling/gamepad-events.c
 
-/*
- * This example code looks for gamepad input in the event handler, and
- * reports any changes as a flood of info.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
-
-/* Joysticks are low-level interfaces: there's something with a bunch of
-   buttons, axes and hats, in no understood order or position. This is
-   a flexible interface, but you'll need to build some sort of configuration
-   UI to let people tell you what button, etc, does what. On top of this
-   interface, SDL offers the "gamepad" API, which works with lots of devices,
-   and knows how to map arbitrary buttons and such to look like an
-   Xbox/PlayStation/etc gamepad. This is easier, and better, for many games,
-   but isn't necessarily a good fit for complex apps and hardware. A flight
-   simulator, a realistic racing game, etc, might want the joystick interface
-   instead of gamepads. */
 
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <print>
+
 /* We will use this renderer to draw into this window every frame. */
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
-static SDL_Color colors[64];
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
 
 #define MOTION_EVENT_COOLDOWN 40
 
-typedef struct EventMessage
-{
-    char* str;
-    SDL_Color color;
-    Uint64 start_ticks;
-    struct EventMessage* next;
-} EventMessage;
-
-static EventMessage messages;
-static EventMessage* messages_tail = &messages;
 
 static const char* battery_state_string(SDL_PowerState state)
 {
@@ -62,69 +36,25 @@ static const char* battery_state_string(SDL_PowerState state)
     return "UNKNOWN";
 }
 
-static void add_message(SDL_JoystickID jid, const char* fmt, ...)
-{
-    const SDL_Color* color = &colors[((size_t)jid) % SDL_arraysize(colors)];
-    EventMessage* msg = NULL;
-    char* str = NULL;
-    va_list ap;
-
-    msg = (EventMessage*)SDL_calloc(1, sizeof(*msg));
-    if (!msg)
-    {
-        return; // oh well.
-    }
-
-    va_start(ap, fmt);
-    SDL_vasprintf(&str, fmt, ap);
-    va_end(ap);
-    if (!str)
-    {
-        SDL_free(msg);
-        return; // oh well.
-    }
-
-    msg->str = str;
-    SDL_copyp(&msg->color, color);
-    msg->start_ticks = SDL_GetTicks();
-    msg->next = NULL;
-
-    messages_tail->next = msg;
-    messages_tail = msg;
-}
-
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    int i;
-
-    SDL_SetAppMetadata("Example Input Gamepad Events", "1.0",
-                       "com.example.input-gamepad-events");
-
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
     {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/input/gamepad-events", 640, 480,
+    if (!SDL_CreateWindowAndRenderer("gamepad-events", 640, 480,
                                      SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    colors[0].r = colors[0].g = colors[0].b = colors[0].a = 255;
-    for (i = 1; i < SDL_arraysize(colors); i++)
-    {
-        colors[i].r = SDL_rand(255);
-        colors[i].g = SDL_rand(255);
-        colors[i].b = SDL_rand(255);
-        colors[i].a = 255;
-    }
 
-    add_message(0, "Please plug in a gamepad.");
+    std::print("Please plug in a gamepad.\n");
 
     return SDL_APP_CONTINUE; /* carry on with the program! */
 }
@@ -145,18 +75,18 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
         SDL_Gamepad* gamepad = SDL_OpenGamepad(which);
         if (!gamepad)
         {
-            add_message(which, "Gamepad #%u add, but not opened: %s",
-                        (unsigned int)which, SDL_GetError());
+            std::print("Gamepad #{} add, but not opened: {}\n",
+                       (unsigned int)which, SDL_GetError());
         }
         else
         {
             char* mapping = SDL_GetGamepadMapping(gamepad);
-            add_message(which, "Gamepad #%u ('%s') added", (unsigned int)which,
-                        SDL_GetGamepadName(gamepad));
+            std::print("Gamepad #{} ('{}') added\n", (unsigned int)which,
+                       SDL_GetGamepadName(gamepad));
             if (mapping)
             {
-                add_message(which, "Gamepad #%u mapping: %s",
-                            (unsigned int)which, mapping);
+                std::print("Gamepad #{} mapping: {}\n", (unsigned int)which,
+                           mapping);
                 SDL_free(mapping);
             }
         }
@@ -169,7 +99,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
         {
             SDL_CloseGamepad(gamepad); /* the gamepad was unplugged. */
         }
-        add_message(which, "Gamepad #%u removed", (unsigned int)which);
+        std::print("Gamepad #{} removed\n", (unsigned int)which);
     }
     else if (event->type == SDL_EVENT_GAMEPAD_AXIS_MOTION)
     {
@@ -180,8 +110,8 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
         {
             const SDL_JoystickID which = event->gaxis.which;
             axis_motion_cooldown_time = now + MOTION_EVENT_COOLDOWN;
-            add_message(
-                which, "Gamepad #%u axis %s -> %d", (unsigned int)which,
+            std::print(
+                "Gamepad #{} axis {} -> {}\n", (unsigned int)which,
                 SDL_GetGamepadStringForAxis((SDL_GamepadAxis)event->gaxis.axis),
                 (int)event->gaxis.value);
         }
@@ -190,10 +120,10 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
              (event->type == SDL_EVENT_GAMEPAD_BUTTON_DOWN))
     {
         const SDL_JoystickID which = event->gbutton.which;
-        add_message(which, "Gamepad #%u button %s -> %s", (unsigned int)which,
-                    SDL_GetGamepadStringForButton(
-                        (SDL_GamepadButton)event->gbutton.button),
-                    event->gbutton.down ? "PRESSED" : "RELEASED");
+        std::print("Gamepad #{} button {} -> {}\n", (unsigned int)which,
+                   SDL_GetGamepadStringForButton(
+                       (SDL_GamepadButton)event->gbutton.button),
+                   event->gbutton.down ? "PRESSED" : "RELEASED");
     }
     else if (event->type == SDL_EVENT_JOYSTICK_BATTERY_UPDATED)
     {
@@ -201,10 +131,9 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
         if (SDL_IsGamepad(which))
         { /* this is only reported for joysticks, so make sure this joystick is
              _actually_ a gamepad. */
-            add_message(which, "Gamepad #%u battery -> %s - %d%%",
-                        (unsigned int)which,
-                        battery_state_string(event->jbattery.state),
-                        event->jbattery.percent);
+            std::print("Gamepad #{} battery -> {} - {}\n", (unsigned int)which,
+                       battery_state_string(event->jbattery.state),
+                       event->jbattery.percent);
         }
     }
 
@@ -215,51 +144,13 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
     const Uint64 now = SDL_GetTicks();
-    const float msg_lifetime = 3500.0f; /* milliseconds a message lives for. */
-    EventMessage* msg = messages.next;
+
     float prev_y = 0.0f;
     int winw = 640, winh = 480;
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_GetWindowSize(window, &winw, &winh);
-
-    while (msg)
-    {
-        float x, y;
-        const float life_percent =
-            ((float)(now - msg->start_ticks)) / msg_lifetime;
-        if (life_percent >= 1.0f)
-        { /* msg is done. */
-            messages.next = msg->next;
-            if (messages_tail == msg)
-            {
-                messages_tail = &messages;
-            }
-            SDL_free(msg->str);
-            SDL_free(msg);
-            msg = messages.next;
-            continue;
-        }
-        x = (((float)winw) -
-             (SDL_strlen(msg->str) * SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE)) /
-            2.0f;
-        y = ((float)winh) * life_percent;
-        if ((prev_y != 0.0f) &&
-            ((prev_y - y) < ((float)SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE)))
-        {
-            msg->start_ticks = now;
-            break; // wait for the previous message to tick up a little.
-        }
-
-        SDL_SetRenderDrawColor(
-            renderer, msg->color.r, msg->color.g, msg->color.b,
-            (Uint8)(((float)msg->color.a) * (1.0f - life_percent)));
-        SDL_RenderDebugText(renderer, x, y, msg->str);
-
-        prev_y = y;
-        msg = msg->next;
-    }
 
     SDL_RenderPresent(renderer);
 
